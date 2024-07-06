dc03cd4d667b5ddb70b46463c83805f0a5bcf57c
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-07-05 23:24:56 -0300

    Add files via upload

---
+++ exercicio8/script.js
@@ -0,0 +1,12 @@
+// Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para imprimir a respectiva mensagem.
+
+let numero = prompt("Digite um número para saber se ele é positivo, negativo ou zero");
+
+if (numero > 0){
+    alert("O número é positivo");
+} else if(numero < 0){
+    alert("O número é negativo");
+} else{
+    alert("O número é zero");
+}
+
